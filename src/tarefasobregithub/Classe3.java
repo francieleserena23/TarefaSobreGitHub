2cef153e8333a6516326522c07954b92a3adc3f3
Franciele Serena <francieleserena@hotmail.com>
2019-10-18 21:12:45 -0300

    Commit da tarefa realizada na aula

--- src/tarefasobregithub/Classe3.java
+++ src/tarefasobregithub/Classe3.java
@@ -16,6 +16,7 @@ public class Classe3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Tarefa sobre GIT HUB");
     }
     
 }
